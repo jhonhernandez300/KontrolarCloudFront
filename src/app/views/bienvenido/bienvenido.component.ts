268450d00cd8ea2d059e510c3c51b097b8aa1552
Jhon Jairo Hernandez Pulgarin <jhon@appenate.com>
2024-08-21 12:00:02 -0500

    feat: i estilos globales

--- src/app/views/bienvenido/bienvenido.component.ts
+++ src/app/views/bienvenido/bienvenido.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, Renderer2  } from '@angular/core';
 
 @Component({
   selector: 'app-bienvenido',
   templateUrl: './bienvenido.component.html',
-  styleUrl: './bienvenido.component.css'
+  styleUrls: ['./bienvenido.component.css']
 })
 export class BienvenidoComponent {
+  constructor(
+    private cdr: ChangeDetectorRef,
+    private renderer: Renderer2
+  ) { } 
 
+  currentTheme: string = 'dark'; 
+
+  changeTheme(theme: string) {    
+    this.currentTheme = theme === 'light' ? 'theme-light' : 'theme-dark'; 
+    this.cdr.detectChanges();
+  }  
 }
